dbc36af98d24db05a671b3599aa7f18aa564679e
Taise <59153204+taise-hub@users.noreply.github.com>
2023-05-21 16:15:55 +0900

    Update viewletter.js

--- viewletter.js
+++ viewletter.js
@@ -1,4 +1,5 @@
-const url = 'https://134.122.109.198:31655/message/4'
+// const url = 'https://134.122.109.198:31655/message/4'
+const url = '/message/3'
 fetch(url, {
         method: 'GET',
         credentials: "include",
